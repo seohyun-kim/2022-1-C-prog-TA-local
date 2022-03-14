ae64f41c7dc798ab2c63f2cd130a2edf84d25fc4
seohyun-kim <selenium34@inu.ac.kr>
2022-03-14 23:20:18 +0900

    초기 커밋.

---
+++ C_TA(2022-1)/OJ1302.c
@@ -0,0 +1,12 @@
+/*
+온라인 저지 1302번: 연산자 우선순위
+http://eseoj.inu.ac.kr/JudgeOnline/problem.php?id=1302
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("%d", 67 - 75 * 3);
+
+    return 0;
+}
---
+++ C_TA(2022-1)/OJ1303.c
@@ -0,0 +1,12 @@
+/*
+온라인 저지 1303번: 괄호에 의한 우선순위 변경
+http://eseoj.inu.ac.kr/JudgeOnline/problem.php?id=1303
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("%d", (67 - 75) * 3);
+
+    return 0;
+}
